fba735bd9b4104cc833e9b3cffbada93a6cf6cc8
Igor Chevdar <igor.chevdar@jetbrains.com>
2026-01-27 13:21:09 +0200

    Revert "wip"
    This reverts commit f371456717b07f52db248841459409bc6fec3011.

--- native/native.tests/testData/standalone/kt56048.kt
+++ native/native.tests/testData/standalone/kt56048.kt
@@ -1,6 +1,6 @@
 // TARGET_BACKEND: NATIVE
 // EXIT_CODE: !0
-// OUTPUT_REGEX: .*Converting Obj-C blocks with non-reference-typed return value to kotlin.Any is not supported \(v\).*Konan_main.*
+// OUTPUT_REGEX: .*Converting Obj-C blocks with non-reference-typed return value to kotlin.Any is not supported \(v\).*kfun:#main.*
 
 // MODULE: cinterop
 // FILE: objclib.def
