899111b4aff27458b4e94af8d37ad749271ddfdd
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-01-28 15:20:59 +0100

    [K/N][performance] Add convertReport to :benchmarksReports
    ^KT-83283

--- kotlin-native/performance/benchmarksReports/build.gradle.kts
+++ kotlin-native/performance/benchmarksReports/build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.benchmarkingTargets
 
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization")
 }
 
 kotlin {
     benchmarkingTargets()
+    jvm()
 
     sourceSets {
         commonMain {
@@ -13,6 +15,12 @@ kotlin {
                 implementation(kotlin("stdlib"))
             }
             kotlin.srcDir("../reports/src/main/kotlin/report")
+        }
+        jvmMain {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
+            }
         }
     }
 }
